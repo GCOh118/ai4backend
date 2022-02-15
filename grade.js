c6ac5952af31458ab176250bd306228ab630c141
GyeoungChan <rudcks118@naver.com>
2022-02-15 14:01:46 +0900

    import, export

---
+++ grade.js
@@ -0,0 +1,9 @@
+export const kor = 90;
+export const eng = 60;
+export const mat = 70;
+
+export const grade = {
+    kor: 90,
+    eng: 60,
+    mat: 80
+}
---
+++ processing.js
@@ -0,0 +1,7 @@
+
+import {grade} from './grade.js'
+
+console.log(grade)
+console.log(grade.kor)
+console.log(grade.eng)
+console.log(grade.mat)
